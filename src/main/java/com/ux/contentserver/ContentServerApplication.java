3762f660b26c2498ed0646a2834128c459f9806e
mike-akluzok <mike@karve.tectury.io>
2021-02-21 18:17:03 +0200

    init

---
+++ src/main/java/com/ux/contentserver/ContentServerApplication.java
@@ -0,0 +1,13 @@
+package com.ux.contentserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContentServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ux/contentserver/ContentServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ux.contentserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
